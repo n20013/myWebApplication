0c01008d648b58fb117aee769d7b6d79425b8d17
= <=>
2021-01-13 05:10:38 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './index.css'
 
-function App() {
+const MoneyBook = props => {
+  const books = [
+    { date: '1/1', item: 'お年玉', amount: 10000 },
+    { date: '1/3', item: 'ケーキ', amount: -500 },
+    { date: '2/1', item: '小遣い', amount: 3000 },
+    { date: '2/5', item: 'マンガ', amount: -600 }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>小遣い帳</h1>
+      <table className='book'>
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>項目</th>
+            <th>入金</th>
+            <th>出金</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{books[0].date}</td>
+            <td>{books[0].item}</td>
+            <td>{books[0].amount}</td>
+          </tr>
+          <tr>
+            <td>{books[1].date}</td>
+            <td>{books[1].item}</td>
+            <td>{-books[1].amount}</td>
+          </tr>
+          <tr>
+            <td>{books[2].date}</td>
+            <td>{books[2].item}</td>
+            <td>{books[2].amount}</td>
+          </tr>
+          <tr>
+            <td>{books[3].date}</td>
+            <td>{books[3].item}</td>
+            <td>{-books[3].amount}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default MoneyBook
